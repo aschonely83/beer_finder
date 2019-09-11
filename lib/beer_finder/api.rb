9967f6a277b497443ee49de88bc77f1da28113d7
Adam Schonely <aschonely@gmail.com>
2019-09-10 21:21:46 -0400

    Updated api and console

--- lib/beer_finder/api.rb
+++ lib/beer_finder/api.rb
@@ -1,4 +1,20 @@
 module BeerFinder
     class API
+      attr_reader :name, :state
+
+      def initialize(name, state)
+        @name = name
+        @state = state
+      end
+      
+      def get_name
+        
+      end
+      
+      def get_state
+
+      end  
     end
+
+
 end
